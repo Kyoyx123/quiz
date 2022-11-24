82c6139e4c1d3e7eefe75b2222a51c65823b3eeb
Kyoyx123 <117514909+Kyoyx123@users.noreply.github.com>
2022-11-24 13:53:25 +0700

    Add files via upload

---
+++ create-quiz.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สร้างแบบทอสอบ</title>
+    <link rel="stylesheet" href="create-quiz.css">
+</head>
+<body>
+    <div class="header">
+        <div class="sub-header">
+
+            <div class="text-create">
+                <p>เริ่มสร้างแบบทดสอบ</p>
+            </div>
+            
+            <div class="box-create">
+                <a href="create-quiz-wang.php">
+                    <div class="box1"></div>
+                </a>
+                <a href="">
+                    <div class="box2"></div>
+                </a>
+                <a href="">
+                    <div class="box3"></div>
+                </a>
+                <a href="">
+                    <div class="box4"></div>
+                </a>
+                <a href="">
+                    <div class="box5"></div>
+                </a>
+                
+            </div>
+            <div class="text-under-box">
+                <p>ว่าง</p>
+                <p>ภาษาไทย</p>
+                <p>คณิตศาสตร์</p>
+                <p>วิทยาศาสตร์</p>
+                <p>ภาษาอังกฤษ</p>
+            </div>
+        </div>
+        </div>
+        <div class="footer">
+
+            </div>
+</body>
+</html>
